8c348f413d06194a9e141228b2176c4a9f1dcb03
rbgksqkr <rbgks1937@naver.com>
2024-09-28 22:32:58 +0900

    refactor: fetchThenRender 코드 작성

--- src/components/TodoInfo.tsx
+++ src/components/TodoInfo.tsx
@@ -1,63 +1,22 @@
-import { useEffect, useState } from "react";
-
-interface Todo {
+export interface Todo {
   completed: boolean;
   id: number;
   title: string;
   userId: number;
 }
 
-const TodoInfo = ({ id }: { id: number }) => {
+const TodoInfo = ({ todo }: { todo: Todo | null }) => {
   console.log("TodoInfo render");
-  const { data, isLoading } = useTodoInfo(id);
 
-  if (isLoading) {
+  if (!todo) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <div>{data?.title}</div>
+      <div>{todo?.title}</div>
     </div>
   );
 };
 
 export default TodoInfo;
-
-const getTodo = async (id: number) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-  const data = await res.json();
-  return data;
-};
-
-const useTodoInfo = (id: number) => {
-  const [data, setData] = useState<Todo>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const fetchTodo = async () => {
-      try {
-        setIsLoading(true);
-        const user = await getTodo(id);
-
-        if (!ignore) {
-          setData(user);
-        }
-
-        setIsLoading(false);
-      } catch (err) {
-        const error = err as Error;
-        setError(error.message);
-      }
-    };
-
-    let ignore = false;
-    fetchTodo();
-    return () => {
-      ignore = true;
-    };
-  }, [id]);
-
-  return { data, isLoading, error };
-};
--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,5 +1,26 @@
 import { useEffect, useState } from "react";
-import TodoInfo from "./TodoInfo";
+import TodoInfo, { Todo } from "./TodoInfo";
+
+const getTodo = async (id: number) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  const data = await res.json();
+  return data;
+};
+
+const getUser = async (id: number) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+  return data;
+};
+
+const fetchUserAndPost = async (userId: number, todoId: number) => {
+  const [user, todo] = await Promise.all([getUser(userId), getTodo(todoId)]);
+
+  return { user, todo };
+};
+
+const promise = ({ userId, todoId }: { userId: number; todoId: number }) =>
+  fetchUserAndPost(userId, todoId);
 
 interface UserInfo {
   id: number;
@@ -9,75 +30,29 @@ interface UserInfo {
 
 export const UserInfo = ({ id }: { id: number }) => {
   console.log("UserInfo render");
-  const { data } = useUserInfo(id);
 
-  if (!data) {
+  const [user, setUser] = useState<UserInfo | null>(null);
+  const [todo, setTodo] = useState<Todo | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    promise({ userId: id, todoId: 2 }).then((data) => {
+      setUser(data.user);
+      setTodo(data.todo);
+      setIsLoading(false);
+    });
+  }, [id]);
+
+  if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <h1>name: {data?.name}</h1>
-      <h2>Email: {data?.email}</h2>
-      <TodoInfo id={2} />
+      <h1>name: {user?.name}</h1>
+      <h2>Email: {user?.email}</h2>
+      <TodoInfo todo={todo} />
     </div>
   );
-};
-
-// const useUserInfo = (id: number): UserInfo => {
-//   if (!cache[id]) {
-//     const promise = getUser(id).then((data) => {
-//       console.log("resolve promise");
-//       cache[id] = { data };
-//     });
-
-//     console.log("throw promise");
-//     cache[id] = { promise };
-//     throw promise;
-//   }
-
-//   if (cache[id].promise) {
-//     throw cache[id].promise;
-//   }
-
-//   console.log("return cache data", cache[id].data!);
-//   return cache[id].data!;
-// };
-
-const useUserInfo = (id: number) => {
-  const [data, setData] = useState<UserInfo>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        setIsLoading(true);
-        const user = await getUser(id);
-
-        if (!ignore) {
-          setData(user);
-        }
-
-        setIsLoading(false);
-      } catch (err) {
-        const error = err as Error;
-        setError(error.message);
-      }
-    };
-
-    let ignore = false;
-    fetchUser();
-    return () => {
-      ignore = true;
-    };
-  }, [id]);
-
-  return { data, isLoading, error };
-};
-
-const getUser = async (id: number) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-  const data = await res.json();
-  return data;
 };
